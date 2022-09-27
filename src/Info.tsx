2843dcb6eebb67593052211617c6daa8c276987c
Guy Frimerman <gfrimerman@gmail.com>
2022-09-27 16:19:07 +0300

    Add grandpa credits

--- src/Info.tsx
+++ src/Info.tsx
@@ -10,15 +10,17 @@ export default function Info() {
 
             <Modal isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay />
-                <ModalContent dir="rtl">
+                <ModalContent m="5" dir="rtl">
                     <ModalHeader>עזרה</ModalHeader>
                     <ModalBody>
                     התשבץ התלת ממדי בנוי לפי כל כללי "תשבץ אמיתי". כדי להקל על פתרונו ממוספרות כל המשבצות לפי סדר, מימין לשמאל, שורה אחר שורה, וקומה אחר קומה. לדוגמה 1 אופקי - משבצות 1 - 7, 1 אנכי - משבצות 1, 8, 15, 22, 1 כלפי מטה – משבצות 197,148,99,50,1, 295,246. פתירת התשבץ אינה כה קשה כפי שהדבר נראה ממבט ראשון ואנו משוכנעים שפתירתו תהווה אתגר יוצא דופן לכל חובב תשבצים מושבע.
+                    <br></br>
+                    התשבץ התלת-מימדי נוצר ב1985 על ידי דב פרימרמן
                     </ModalBody>
 
                     <ModalFooter>
                         <Button colorScheme='blue' mr={3} onClick={onClose}>
-                            Close
+                            חזרה
                         </Button>
                     </ModalFooter>
                 </ModalContent>
